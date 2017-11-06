8aa74ad77d7033423b58266c3e4ce165377d479c
DanielCKennedy <danielckennedy@hotmail.com>
2017-11-05 19:36:13 -0800

    initial commit

--- KinectTest2/Form1.Designer.cs
+++ KinectTest2/Form1.Designer.cs
@@ -28,12 +28,165 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.rightHandX = new System.Windows.Forms.Label();
+            this.rightHandY = new System.Windows.Forms.Label();
+            this.rightHandZ = new System.Windows.Forms.Label();
+            this.rightHipX = new System.Windows.Forms.Label();
+            this.rightHipY = new System.Windows.Forms.Label();
+            this.rightHipZ = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // rightHandX
+            // 
+            this.rightHandX.AutoSize = true;
+            this.rightHandX.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F);
+            this.rightHandX.Location = new System.Drawing.Point(76, 36);
+            this.rightHandX.Name = "rightHandX";
+            this.rightHandX.Size = new System.Drawing.Size(24, 24);
+            this.rightHandX.TabIndex = 0;
+            this.rightHandX.Text = "X";
+            // 
+            // rightHandY
+            // 
+            this.rightHandY.AutoSize = true;
+            this.rightHandY.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F);
+            this.rightHandY.Location = new System.Drawing.Point(76, 65);
+            this.rightHandY.Name = "rightHandY";
+            this.rightHandY.Size = new System.Drawing.Size(22, 24);
+            this.rightHandY.TabIndex = 1;
+            this.rightHandY.Text = "Y";
+            // 
+            // rightHandZ
+            // 
+            this.rightHandZ.AutoSize = true;
+            this.rightHandZ.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F);
+            this.rightHandZ.Location = new System.Drawing.Point(76, 102);
+            this.rightHandZ.Name = "rightHandZ";
+            this.rightHandZ.Size = new System.Drawing.Size(22, 24);
+            this.rightHandZ.TabIndex = 2;
+            this.rightHandZ.Text = "Z";
+            // 
+            // rightHipX
+            // 
+            this.rightHipX.AutoSize = true;
+            this.rightHipX.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F);
+            this.rightHipX.Location = new System.Drawing.Point(186, 36);
+            this.rightHipX.Name = "rightHipX";
+            this.rightHipX.Size = new System.Drawing.Size(24, 24);
+            this.rightHipX.TabIndex = 3;
+            this.rightHipX.Text = "X";
+            // 
+            // rightHipY
+            // 
+            this.rightHipY.AutoSize = true;
+            this.rightHipY.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F);
+            this.rightHipY.Location = new System.Drawing.Point(186, 65);
+            this.rightHipY.Name = "rightHipY";
+            this.rightHipY.Size = new System.Drawing.Size(22, 24);
+            this.rightHipY.TabIndex = 4;
+            this.rightHipY.Text = "Y";
+            // 
+            // rightHipZ
+            // 
+            this.rightHipZ.AutoSize = true;
+            this.rightHipZ.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F);
+            this.rightHipZ.Location = new System.Drawing.Point(186, 102);
+            this.rightHipZ.Name = "rightHipZ";
+            this.rightHipZ.Size = new System.Drawing.Size(22, 24);
+            this.rightHipZ.TabIndex = 5;
+            this.rightHipZ.Text = "Z";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F);
+            this.label1.Location = new System.Drawing.Point(76, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(104, 24);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Right Hand";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F);
+            this.label2.Location = new System.Drawing.Point(186, 12);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(87, 24);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Right Hip";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F);
+            this.label3.Location = new System.Drawing.Point(23, 31);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(24, 24);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "X";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F);
+            this.label4.Location = new System.Drawing.Point(23, 65);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(22, 24);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Y";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F);
+            this.label5.Location = new System.Drawing.Point(23, 102);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(22, 24);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Z";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(478, 352);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.rightHipZ);
+            this.Controls.Add(this.rightHipY);
+            this.Controls.Add(this.rightHipX);
+            this.Controls.Add(this.rightHandZ);
+            this.Controls.Add(this.rightHandY);
+            this.Controls.Add(this.rightHandX);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label rightHandX;
+        private System.Windows.Forms.Label rightHandY;
+        private System.Windows.Forms.Label rightHandZ;
+        private System.Windows.Forms.Label rightHipX;
+        private System.Windows.Forms.Label rightHipY;
+        private System.Windows.Forms.Label rightHipZ;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
     }
 }
 
--- KinectTest2/Form1.cs
+++ KinectTest2/Form1.cs
@@ -7,14 +7,88 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Microsoft.Kinect;
 
 namespace KinectTest2
 {
     public partial class Form1 : Form
     {
+        KinectSensor kinectSensor = null;
+        BodyFrameReader bodyFrameReader = null;
+        Body[] bodies = null;
+
+        bool recordReady = false;
+        bool recording = false;
+        LinkedList<CameraSpacePoint> recordedPoints = new LinkedList<CameraSpacePoint>();
+        CameraSpacePoint prevPoint;
+
         public Form1()
         {
             InitializeComponent();
+            initializeKinect();
+        }
+
+        public void initializeKinect()
+        {
+            kinectSensor = KinectSensor.GetDefault();
+
+            if (kinectSensor != null)
+            {
+                // turn on kinect
+                kinectSensor.Open();
+            }
+
+            bodyFrameReader = kinectSensor.BodyFrameSource.OpenReader();
+
+            if (bodyFrameReader != null)
+            {
+                bodyFrameReader.FrameArrived += Reader_FrameArrived;
+            }
+        }
+
+        private void Reader_FrameArrived(object sender, BodyFrameArrivedEventArgs e)
+        {
+            bool dataReceived = false;
+
+            using (BodyFrame bodyFrame = e.FrameReference.AcquireFrame())
+            {
+                if (bodyFrame != null)
+                {
+                    if (bodies == null)
+                    {
+                        bodies = new Body[bodyFrame.BodyCount];
+                    }
+                    bodyFrame.GetAndRefreshBodyData(bodies);
+                    dataReceived = true;
+                }
+
+                if (dataReceived)
+                {
+                    foreach (Body body in bodies)
+                    {
+                        if (body.IsTracked)
+                        {
+                            IReadOnlyDictionary<JointType, Joint> joints = body.Joints;
+                            Dictionary<JointType, Point> jointPoints = new Dictionary<JointType, Point>();
+
+                            Joint rightHand = joints[JointType.HandRight];
+                            Joint rightHip = joints[JointType.HipRight];
+                            Joint rightElbow = joints[JointType.ElbowRight];
+
+                            rightHandX.Text = rightHand.Position.X.ToString("#.##");
+                            rightHandY.Text = rightHand.Position.Y.ToString("#.##");
+                            rightHandZ.Text = rightHand.Position.Z.ToString("#.##");
+
+                            rightHipX.Text = rightHip.Position.X.ToString("#.##");
+                            rightHipY.Text = rightHip.Position.Y.ToString("#.##");
+                            rightHipZ.Text = rightHip.Position.Z.ToString("#.##");
+
+                            Console.Out.WriteLine($"Hand: {rightHand.Position.X} {rightHand.Position.Y} {rightHand.Position.Z}");
+                            Console.Out.WriteLine($"Hip: {rightHip.Position.X} {rightHip.Position.Y} {rightHip.Position.Z}");
+                        }
+                    }
+                }
+            }
         }
     }
 }
