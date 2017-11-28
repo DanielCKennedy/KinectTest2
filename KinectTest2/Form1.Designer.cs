af500e9c39f22aa2ee9956bdd826e05706d65552
matth <matthew.stienhardt@gmail.com>
2017-11-28 13:27:37 -0800

    Added some kewl shiz

--- KinectTest2/Form1.Designer.cs
+++ KinectTest2/Form1.Designer.cs
@@ -39,15 +39,28 @@
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.recodingModeLabel = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.isReleasedLabel = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.dzLabel = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.dyLabel = new System.Windows.Forms.Label();
+            this.labelXX = new System.Windows.Forms.Label();
+            this.velocityLabel = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.angleLabel = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // rightHandX
             // 
             this.rightHandX.AutoSize = true;
             this.rightHandX.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F);
-            this.rightHandX.Location = new System.Drawing.Point(76, 36);
+            this.rightHandX.Location = new System.Drawing.Point(152, 69);
+            this.rightHandX.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.rightHandX.Name = "rightHandX";
-            this.rightHandX.Size = new System.Drawing.Size(24, 24);
+            this.rightHandX.Size = new System.Drawing.Size(45, 44);
             this.rightHandX.TabIndex = 0;
             this.rightHandX.Text = "X";
             // 
@@ -55,9 +68,10 @@
             // 
             this.rightHandY.AutoSize = true;
             this.rightHandY.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F);
-            this.rightHandY.Location = new System.Drawing.Point(76, 65);
+            this.rightHandY.Location = new System.Drawing.Point(152, 125);
+            this.rightHandY.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.rightHandY.Name = "rightHandY";
-            this.rightHandY.Size = new System.Drawing.Size(22, 24);
+            this.rightHandY.Size = new System.Drawing.Size(44, 44);
             this.rightHandY.TabIndex = 1;
             this.rightHandY.Text = "Y";
             // 
@@ -65,9 +79,10 @@
             // 
             this.rightHandZ.AutoSize = true;
             this.rightHandZ.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F);
-            this.rightHandZ.Location = new System.Drawing.Point(76, 102);
+            this.rightHandZ.Location = new System.Drawing.Point(152, 196);
+            this.rightHandZ.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.rightHandZ.Name = "rightHandZ";
-            this.rightHandZ.Size = new System.Drawing.Size(22, 24);
+            this.rightHandZ.Size = new System.Drawing.Size(43, 44);
             this.rightHandZ.TabIndex = 2;
             this.rightHandZ.Text = "Z";
             // 
@@ -75,9 +90,10 @@
             // 
             this.rightHipX.AutoSize = true;
             this.rightHipX.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F);
-            this.rightHipX.Location = new System.Drawing.Point(186, 36);
+            this.rightHipX.Location = new System.Drawing.Point(372, 69);
+            this.rightHipX.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.rightHipX.Name = "rightHipX";
-            this.rightHipX.Size = new System.Drawing.Size(24, 24);
+            this.rightHipX.Size = new System.Drawing.Size(45, 44);
             this.rightHipX.TabIndex = 3;
             this.rightHipX.Text = "X";
             // 
@@ -85,9 +101,10 @@
             // 
             this.rightHipY.AutoSize = true;
             this.rightHipY.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F);
-            this.rightHipY.Location = new System.Drawing.Point(186, 65);
+            this.rightHipY.Location = new System.Drawing.Point(372, 125);
+            this.rightHipY.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.rightHipY.Name = "rightHipY";
-            this.rightHipY.Size = new System.Drawing.Size(22, 24);
+            this.rightHipY.Size = new System.Drawing.Size(44, 44);
             this.rightHipY.TabIndex = 4;
             this.rightHipY.Text = "Y";
             // 
@@ -95,9 +112,10 @@
             // 
             this.rightHipZ.AutoSize = true;
             this.rightHipZ.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F);
-            this.rightHipZ.Location = new System.Drawing.Point(186, 102);
+            this.rightHipZ.Location = new System.Drawing.Point(372, 196);
+            this.rightHipZ.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.rightHipZ.Name = "rightHipZ";
-            this.rightHipZ.Size = new System.Drawing.Size(22, 24);
+            this.rightHipZ.Size = new System.Drawing.Size(43, 44);
             this.rightHipZ.TabIndex = 5;
             this.rightHipZ.Text = "Z";
             // 
@@ -105,9 +123,10 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F);
-            this.label1.Location = new System.Drawing.Point(76, 12);
+            this.label1.Location = new System.Drawing.Point(152, 23);
+            this.label1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(104, 24);
+            this.label1.Size = new System.Drawing.Size(209, 44);
             this.label1.TabIndex = 6;
             this.label1.Text = "Right Hand";
             // 
@@ -115,19 +134,21 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F);
-            this.label2.Location = new System.Drawing.Point(186, 12);
+            this.label2.Location = new System.Drawing.Point(372, 23);
+            this.label2.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(87, 24);
+            this.label2.Size = new System.Drawing.Size(271, 44);
             this.label2.TabIndex = 7;
-            this.label2.Text = "Right Hip";
+            this.label2.Text = "Right Shoulder";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F);
-            this.label3.Location = new System.Drawing.Point(23, 31);
+            this.label3.Location = new System.Drawing.Point(46, 60);
+            this.label3.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(24, 24);
+            this.label3.Size = new System.Drawing.Size(45, 44);
             this.label3.TabIndex = 8;
             this.label3.Text = "X";
             // 
@@ -135,9 +156,10 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F);
-            this.label4.Location = new System.Drawing.Point(23, 65);
+            this.label4.Location = new System.Drawing.Point(46, 125);
+            this.label4.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(22, 24);
+            this.label4.Size = new System.Drawing.Size(44, 44);
             this.label4.TabIndex = 9;
             this.label4.Text = "Y";
             // 
@@ -145,17 +167,135 @@
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F);
-            this.label5.Location = new System.Drawing.Point(23, 102);
+            this.label5.Location = new System.Drawing.Point(46, 196);
+            this.label5.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(22, 24);
+            this.label5.Size = new System.Drawing.Size(43, 44);
             this.label5.TabIndex = 10;
             this.label5.Text = "Z";
             // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(648, 42);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(199, 25);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "In Recording Mode:";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // recodingModeLabel
+            // 
+            this.recodingModeLabel.AutoSize = true;
+            this.recodingModeLabel.Location = new System.Drawing.Point(653, 87);
+            this.recodingModeLabel.Name = "recodingModeLabel";
+            this.recodingModeLabel.Size = new System.Drawing.Size(43, 25);
+            this.recodingModeLabel.TabIndex = 12;
+            this.recodingModeLabel.Text = "NO";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(653, 196);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(131, 25);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "Is Released:";
+            // 
+            // isReleasedLabel
+            // 
+            this.isReleasedLabel.AutoSize = true;
+            this.isReleasedLabel.Location = new System.Drawing.Point(653, 239);
+            this.isReleasedLabel.Name = "isReleasedLabel";
+            this.isReleasedLabel.Size = new System.Drawing.Size(43, 25);
+            this.isReleasedLabel.TabIndex = 14;
+            this.isReleasedLabel.Text = "NO";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(653, 347);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(35, 25);
+            this.label8.TabIndex = 15;
+            this.label8.Text = "dz";
+            // 
+            // dzLabel
+            // 
+            this.dzLabel.AutoSize = true;
+            this.dzLabel.Location = new System.Drawing.Point(658, 391);
+            this.dzLabel.Name = "dzLabel";
+            this.dzLabel.Size = new System.Drawing.Size(0, 25);
+            this.dzLabel.TabIndex = 16;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(734, 347);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(35, 25);
+            this.label10.TabIndex = 17;
+            this.label10.Text = "dy";
+            // 
+            // dyLabel
+            // 
+            this.dyLabel.AutoSize = true;
+            this.dyLabel.Location = new System.Drawing.Point(734, 391);
+            this.dyLabel.Name = "dyLabel";
+            this.dyLabel.Size = new System.Drawing.Size(0, 25);
+            this.dyLabel.TabIndex = 18;
+            // 
+            // labelXX
+            // 
+            this.labelXX.AutoSize = true;
+            this.labelXX.Location = new System.Drawing.Point(54, 347);
+            this.labelXX.Name = "labelXX";
+            this.labelXX.Size = new System.Drawing.Size(202, 25);
+            this.labelXX.TabIndex = 19;
+            this.labelXX.Text = "Calculated Velocity:";
+            // 
+            // velocityLabel
+            // 
+            this.velocityLabel.AutoSize = true;
+            this.velocityLabel.Location = new System.Drawing.Point(277, 347);
+            this.velocityLabel.Name = "velocityLabel";
+            this.velocityLabel.Size = new System.Drawing.Size(0, 25);
+            this.velocityLabel.TabIndex = 20;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(75, 403);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(181, 25);
+            this.label9.TabIndex = 21;
+            this.label9.Text = "Calculated Angle:";
+            // 
+            // angleLabel
+            // 
+            this.angleLabel.AutoSize = true;
+            this.angleLabel.Location = new System.Drawing.Point(282, 403);
+            this.angleLabel.Name = "angleLabel";
+            this.angleLabel.Size = new System.Drawing.Size(0, 25);
+            this.angleLabel.TabIndex = 22;
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(478, 352);
+            this.ClientSize = new System.Drawing.Size(956, 677);
+            this.Controls.Add(this.angleLabel);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.velocityLabel);
+            this.Controls.Add(this.labelXX);
+            this.Controls.Add(this.dyLabel);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.dzLabel);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.isReleasedLabel);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.recodingModeLabel);
+            this.Controls.Add(this.label6);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
@@ -167,6 +307,7 @@
             this.Controls.Add(this.rightHandZ);
             this.Controls.Add(this.rightHandY);
             this.Controls.Add(this.rightHandX);
+            this.Margin = new System.Windows.Forms.Padding(6);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -187,6 +328,18 @@
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label recodingModeLabel;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label isReleasedLabel;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label dzLabel;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label dyLabel;
+        private System.Windows.Forms.Label labelXX;
+        private System.Windows.Forms.Label velocityLabel;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label angleLabel;
     }
 }
 
--- KinectTest2/Form1.cs
+++ KinectTest2/Form1.cs
@@ -19,8 +19,15 @@ namespace KinectTest2
 
         bool recordReady = false;
         bool recording = false;
+        bool releasedNow = false;
         LinkedList<CameraSpacePoint> recordedPoints = new LinkedList<CameraSpacePoint>();
         CameraSpacePoint prevPoint;
+
+        public static float HAND_SHOULDER_Z_DELTA = 0.05f;
+        public static float HAND_ELBOW_Y_DELTA = 0.01f;
+        public static float TIME_OF_5_FRAMES = 0.17f;
+        private double releasedAngle;
+        private double releasedVelocity;
 
         public Form1()
         {
@@ -74,14 +81,38 @@ namespace KinectTest2
                             Joint rightHand = joints[JointType.HandRight];
                             Joint rightHip = joints[JointType.HipRight];
                             Joint rightElbow = joints[JointType.ElbowRight];
+                            Joint shoulderRight = joints[JointType.ShoulderRight];
+
+                            recording = inRecordingMode(joints) || recording;
+                            releasedNow = isReleased(joints);
+
+                            recodingModeLabel.Text = recording ? "TRUE" : "FALSE";
+                            isReleasedLabel.Text = releasedNow ? "TRUE" : "FALSE";
+
+                            if (recording)
+                                recordedPoints.AddLast(rightHand.Position);
+
+                            if (recording && releasedNow)
+                            {
+                                releasedAngle = calculateAngle();
+                                releasedVelocity = calculateVelocity();
+                                recordedPoints.Clear();
+                                recording = false;
+
+                                velocityLabel.Text = releasedVelocity.ToString("#.##");
+
+                            }
+
+
+                            recordedPoints.AddLast(rightHand.Position);
 
                             rightHandX.Text = rightHand.Position.X.ToString("#.##");
                             rightHandY.Text = rightHand.Position.Y.ToString("#.##");
                             rightHandZ.Text = rightHand.Position.Z.ToString("#.##");
 
-                            rightHipX.Text = rightHip.Position.X.ToString("#.##");
-                            rightHipY.Text = rightHip.Position.Y.ToString("#.##");
-                            rightHipZ.Text = rightHip.Position.Z.ToString("#.##");
+                            rightHipX.Text = shoulderRight.Position.X.ToString("#.##");
+                            rightHipY.Text = shoulderRight.Position.Y.ToString("#.##");
+                            rightHipZ.Text = shoulderRight.Position.Z.ToString("#.##");
 
                             Console.Out.WriteLine($"Hand: {rightHand.Position.X} {rightHand.Position.Y} {rightHand.Position.Z}");
                             Console.Out.WriteLine($"Hip: {rightHip.Position.X} {rightHip.Position.Y} {rightHip.Position.Z}");
@@ -89,6 +120,61 @@ namespace KinectTest2
                     }
                 }
             }
+        }
+
+        private double calculateVelocity()
+        {
+            double slope = 0;
+            int len = recordedPoints.Count - 1;
+            int numFrames = 5;
+            int i;
+            for (i = 5; i >= 0; i++)
+            {
+                if (len - i < 0)
+                    break;
+
+                slope += recordedPoints.ElementAt(len - i).Y / recordedPoints.ElementAt(len - i).Z;
+            }
+
+            slope /= numFrames - i;
+
+            return slope / TIME_OF_5_FRAMES;
+        }
+
+        private double calculateAngle()
+        {
+            return 0;
+        }
+
+        private bool inRecordingMode(IReadOnlyDictionary<JointType, Joint> joints)
+        {
+            Joint rightHand = joints[JointType.HandRight];
+            Joint rightElbow = joints[JointType.ElbowRight];
+            Joint shoulderRight = joints[JointType.ShoulderRight];
+
+            return rightHand.Position.Z > (shoulderRight.Position.Z + HAND_SHOULDER_Z_DELTA)
+                && rightHand.Position.Y < (rightElbow.Position.Y + HAND_ELBOW_Y_DELTA);
+        }
+
+        private bool isReleased(IReadOnlyDictionary<JointType, Joint> joints)
+        {
+            Joint rightHand = joints[JointType.HandRight];
+            Joint rightElbow = joints[JointType.ElbowRight];
+            Joint shoulderRight = joints[JointType.ShoulderRight];
+            //Joint hip = joints[JointType.HipRight];
+
+            double dz = shoulderRight.Position.Z - rightHand.Position.Z;
+            double dy = shoulderRight.Position.Y - rightHand.Position.Y;
+            dzLabel.Text = dz.ToString("#.##");
+            dyLabel.Text = dy.ToString("#.##");
+            bool isReleased = dz >= dy;
+
+            return isReleased;
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
